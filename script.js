b4e7f4bfd8d6d8acf61526a12cb6173c3d07b62c
AlexCode-dot <alle7000.andersson@gmail.com>
2024-10-03 22:20:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+const input = document.querySelector("#input-value");
+const listContainer = document.querySelector("#list-container");
+const taskCount = document.querySelector("#task-count");
+const addBtn = document.querySelector("#add");
+const warningText = document.querySelector("#warning-text");
+
+addBtn.addEventListener(
+    "click",
+    addTask,
+    false
+);
+
+function addTask() 
+{
+    if(input.value === "") 
+        {
+            warningText.innerHTML = "Input must not be empty";
+            warningText.setAttribute("id", "blink-animation");
+        }
+        else
+        {
+            warningText.removeAttribute("id");
+            warningText.innerHTML = "";
+            const li = document.createElement("li");
+            li.innerHTML = input.value;
+            listContainer.appendChild(li);
+            const span = document.createElement("span");
+            span.innerHTML = "&#128465;";
+            li.appendChild(span);
+        }
+        input.value = "";
+        arrayFunction();
+        saveData();
+}
+
+listContainer.addEventListener("click", function(e)
+{
+    if (e.target.tagName === "LI")
+        {
+            e.target.classList.toggle("checked");
+        
+        }
+        else if (e.target.tagName === "SPAN")
+        {
+            e.target.parentElement.remove();
+            
+        }
+        arrayFunction();
+        removeOne();
+        saveData();
+});
+
+function arrayFunction() 
+{
+    const toDos = [];
+
+    for (let i = 0; i < listContainer.children.length; i++) {
+        const toDo = listContainer.children.item(i);
+
+        const toDoInfo = 
+        {
+            "task": toDo.innerText,
+            "completed": toDo.classList.contains("checked")
+        };
+        
+        toDos.push(toDoInfo);
+
+        resultCounter(toDos);
+    }
+    console.log(toDos);
+}
+
+function resultCounter(toDos)
+{
+const taskCompleted = toDos.filter(task => task.completed);
+const numberCompletedTasks = taskCompleted.length;
+taskCount.textContent = numberCompletedTasks;
+console.log(numberCompletedTasks);
+}
+
+function removeOne()
+{
+    if (listContainer.children.length === 0)
+        {
+            taskCount.textContent = 0;
+        }
+        
+}
+
+
+function saveData()
+{
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask()
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
